396d287b381efade1e38e8280ea92d33545c8f36
Joseph Kinsella <jkinsella@spiryx.net>
2013-10-07 05:25:09 -0400

    Common factors.

--- src/prime/prime.c
+++ src/prime/prime.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
 #include <math.h>
+
+int64_t * common_factors(int64_t n, size_t * size)
+{
+	int64_t * tmp, max, i, * result = NULL;
+
+	if(n == 0)
+		return NULL;
+	
+	*size = 0;
+	max = sqrtl(n) + 1;
+	for(i = 1; i <= max; ++i) {
+		if((n % i) == 0) {
+			*size += 2;
+			tmp = realloc(result, (*size) * sizeof(int64_t));
+			if(tmp == NULL) {
+				if(result)
+					free(result);
+				fprintf(stderr, "Not enough memory\n");
+				return NULL;
+			}
+			result = tmp;
+			result[(*size)-2] = i;
+			result[(*size)-1] = n/i;
+		}
+	}
+	return result;
+}
 
 int is_prime(unsigned long long number)
 {
@@ -26,6 +55,8 @@ int is_prime(unsigned long long number)
 int main(int argc, char *argv[])
 {
 	int i;
+	size_t size, j;
+	uint64_t * f, n;
 
 	if(argc < 2) {
 		printf("Usage: %s <number> [number...]\n", argv[0]);
@@ -33,8 +64,19 @@ int main(int argc, char *argv[])
 	}
 
 	for(i = 1; i < argc; ++i) {
-		printf("%s is %sprime\n", argv[i],
-			is_prime(atoll(argv[i]))? "": "not ");
+		n = atoll(argv[i]);
+		f = common_factors(n, &size);
+		if(f == NULL)
+			continue;
+
+		printf("%" PRIu64 " is %sprime, with factors = { ",
+				n, size == 2? "": "not ");
+
+		for(j = 0; j  < size; j+=2)
+			printf("%" PRIu64 "*%" PRIu64 ", ", f[j], f[j+1]);
+
+		printf("\b\b }\n");
+		free(f);
 	}
 
 	return 0;
