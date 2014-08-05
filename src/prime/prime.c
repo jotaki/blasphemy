fa0d705fab659c480318c44fe815432d55c1a597
Joseph Kinsella <jkinsella@spiryx.net>
2014-08-05 12:46:35 -0400

    meh

--- src/prime/prime.c
+++ src/prime/prime.c
@@ -1,8 +1,51 @@
+/*
+ * code is ugly, should be cleaned up...
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <inttypes.h>
 #include <math.h>
+
+int64_t * prime_factors(int64_t n, size_t * size)
+{
+	int64_t * tmp, i,
+		* result = NULL;
+
+	if(n == 0)
+		return NULL;
+
+	*size = 0;
+	if((n & 1) == 0) { // even (prime 2 is a factor)
+		result = malloc(sizeof(int64_t));
+		if(result == NULL)
+			return NULL;
+
+		result[(*size)++] = 2;
+		n >>= __builtin_ctz(n);	/* divide by two until odd */
+	}
+
+	for(i = 3; n > 1; i += 2) {
+		if((n % i))
+			continue;
+
+		tmp = realloc(result, ++(*size) * sizeof(int64_t));
+		if(tmp == NULL) {
+			if(result)
+				free(result);
+
+			return NULL;
+		}
+		result = tmp;
+		result[(*size)-1] = i;
+
+		for(; n % i == 0; n /= i)
+			/* do nothing */ ;
+	}
+
+	return result;
+}
 
 int64_t * common_factors(int64_t n, size_t * size)
 {
@@ -51,32 +94,63 @@ int is_prime(unsigned long long number)
         return 1;
 }
 
+void usage(const char * const arg0, int code)
+{
+	printf("Usage: %s [options] <number> [number...]\n", arg0);
+	printf("%s version 0.3, available options:\n\n", arg0);
+	printf("    -p    - Print only prime factors\n");
+
+	exit(code);
+}
 
 int main(int argc, char *argv[])
 {
-	int i;
+	int i, pfonly = 0;
 	size_t size, j;
 	int64_t * f, n;
 
+
+	if(argc < 2) { 
+		usage(argv[0], 1);
+	}
+
+	/* XXX: HACK! XXX */
+	if(argv[1][0] == '-' && argv[1][1] == 'p') {
+		++pfonly; ++argv; --argc;
+	}
+
 	if(argc < 2) {
-		printf("Usage: %s <number> [number...]\n", argv[0]);
-		return 1;
+		usage(argv[-1], 1);
 	}
 
 	for(i = 1; i < argc; ++i) {
 		n = atoll(argv[i]);
-		f = common_factors(n, &size);
-		if(f == NULL)
-			continue;
+		if(pfonly == 0) {
+			f = common_factors(n, &size);
+			if(f == NULL)
+				continue;
 
-		printf("%" PRIu64 " is %sprime, with factors = { ",
-				n, size == 2 && n != 1? "": "not ");
+			printf("%" PRIu64 " is %sprime, with factors = { ",
+					n, size == 2 && n != 1? "": "not ");
 
-		for(j = 0; j  < size; j+=2)
-			printf("%" PRIu64 "*%" PRIu64 ", ", f[j], f[j+1]);
+			for(j = 0; j  < size; j+=2)
+				printf("%" PRIu64 "*%" PRIu64 ", ", f[j],
+						f[j+1]);
 
-		printf("\b\b }\n");
-		free(f);
+			printf("\b\b }\n");
+			free(f);
+		} else {
+			f = prime_factors(n, &size);
+			if(f == NULL)
+				continue;
+
+			printf("%" PRIu64 " has prime factors: ", n);
+			for(j = 0; j < size; ++j)
+				printf("%" PRIu64 ", ", f[j]);
+
+			printf("\b\b\n");
+			free(f);
+		}
 	}
 
 	return 0;
